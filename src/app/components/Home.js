2d5225943793671578387d37df326249f93ada6e
abcd <kritiagarwal@Kritis-MacBook-Air.local>
2018-01-21 23:57:55 +0530

    outputting dynamic data

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,9 +2,13 @@ import React from "react"
 
 export class Home extends React.Component {
   render() {
+    let content = "";
+    if(true)
+      content = "HEY";
     return(
       <div>
         <p>In a new Component!</p>
+        {content}
       </div>
     );
   }
