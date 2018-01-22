84d81303d13a112fc4b2170fd4b4517d96e047a2
abcd <kritiagarwal@Kritis-MacBook-Air.local>
2018-01-22 12:00:10 +0530

    learing to use states

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -3,18 +3,22 @@ import React from "react"
 export class Home extends React.Component {
   constructor(props) {
     super();
-    this.age = props.age;
+    this.state = {
+      age: props.age
+    };
   }
   onMakeOlder() {
-    this.age = Number(this.age) + 3;
-    console.log(this.age);
+    this.setState({
+      age: Number(this.state.age) + 3
+    });
+    console.log(this.state.age);
   }
   render() {
     console.log(this.props);
     return(
       <div>
         <p>My name is {this.props.name}</p>
-        <p>My age is {this.age}</p>
+        <p>My age is {this.state.age}</p>
         <hr/>
         <button className="btn btn-primary" onClick={this.onMakeOlder.bind(this)}>Make me older</button>
       </div>
