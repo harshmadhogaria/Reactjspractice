fddf21aa81f9fe305ab7f530b39a3742b4a66f29
abcd <kritiagarwal@Kritis-MacBook-Air.local>
2018-01-22 11:48:33 +0530

    working with events and learning about bind(this)

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,25 +1,22 @@
 import React from "react"
 
 export class Home extends React.Component {
+  constructor(props) {
+    super();
+    this.age = props.age;
+  }
+  onMakeOlder() {
+    this.age = Number(this.age) + 3;
+    console.log(this.age);
+  }
   render() {
     console.log(this.props);
-    let content = "";
-    if(true)
-      content = "HEY";
     return(
       <div>
-        <p>In a new Component!</p>
         <p>My name is {this.props.name}</p>
-        <p>My age is {this.props.age}</p>
-        <p>User object => Nmae : {this.props.user.name}</p>
-        <div>
-          <h4>Hobbies</h4>
-          <ul>
-           {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
-          </ul>
-          <hr/>
-            {this.props.children}
-        </div>
+        <p>My age is {this.age}</p>
+        <hr/>
+        <button className="btn btn-primary" onClick={this.onMakeOlder.bind(this)}>Make me older</button>
       </div>
     );
   }
