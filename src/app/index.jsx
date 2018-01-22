dc9c6e1c1e12758f4b1a85d9af64c16028772a62
abcd <kritiagarwal@Kritis-MacBook-Air.local>
2018-01-22 12:26:10 +0530

    learning stateless components or function components

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -5,22 +5,16 @@ import { Home } from "./components/Home"
 
 class App extends React.Component {
 	render() {
-	var user = {
-		name : "Harsh",
-		hobbies : ["abc", "def", "ghi"]
-	}
 		return (
 			<div className="container">
 				<div className="row">
 					<div className="col-xs-10 col-xs-offset-1">
-						<Header/>
+						<Header homeLink="Home"/>
 					</div>
 				</div>
 				<div className="row">
 					<div className="col-xs-10 col-xs-offset-1">
-						<Home name={"Harsh"} age="21" user={user}>
-							<p>This is a paragraph</p>
-						</Home>
+						<Home name={"Harsh"} age="21" />
 					</div>
 				</div>
 			</div>
